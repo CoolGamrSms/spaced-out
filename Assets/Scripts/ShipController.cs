7e2da6783d341d4b03aecb918b879addbfa5b3e9
yhpham <yelenapham@gmail.com>
2016-11-06 13:31:03 -0500

    add ship movement

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+    public float speed;
+    Vector3 vel;
+
+	private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        vel = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        
+        if (GetArrowInput() && (vel != Vector3.zero)) {
+            transform.rotation = Quaternion.LookRotation(vel);
+        }
+        else {
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+
+        rb.velocity = new Vector3(vel.x, rb.velocity.y, vel.z);
+    }
+
+    bool GetArrowInput() {
+        return (Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0);
+    }
+}
