374b0d38ed54ade08826a8ee0e822722a21a05a9
Amy <acliu@umich.edu>
2016-11-09 23:51:16 -0500

    hull breach prototype

---
+++ Assets/Scripts/ObjectScripts/HullBreach.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HullBreach : ObjectBase {
+
+    //Controlled by ship script
+    public bool broken = false;
+    public float timeLimit = 50f;
+
+    private string inputAxis;
+    private float timer = 0;
+	// Use this for initialization
+	void Start () {
+        // inputAxis = "joystick " + GetComponent<ObjectBase>().JoystickNum + " button 2";
+        this.enabled = false;
+        inputAxis = "joystick 1 button 2";
+	}
+	
+	// Only enabled when Engineer in range
+	void FixedUpdate () {
+        if (Input.GetKey(inputAxis)) {
+            timer += Time.deltaTime;
+            if (timer > timeLimit) {
+                //fixed, remove negative effect on ship
+                print("fixed");
+                timer = 0;
+                gameObject.SetActive(false);
+            }
+
+        }
+	}
+
+    void OnTriggerEnter( Collider col) {
+        if (col.gameObject.tag == "Engineer" && broken) {
+            this.enabled = true;
+        }
+    }
+
+    void OnTriggerExit( Collider col) {
+        if (col.gameObject.tag == "Engineer" && broken){
+            this.enabled = false;
+            timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectScripts/ObjectBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBase : MonoBehaviour {
+
+    //set by ship script
+    public int JoystickNum;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
