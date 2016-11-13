12aa9355d4a3c5cb907fdf26f38a950314e5d5b6
yhpham <yelenapham@gmail.com>
2016-11-13 18:30:56 -0500

    add start scene

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene("scene_0");
+	}
+
+	public void Instructions() {
+		SceneManager.LoadScene("Instructions");
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
