b933b0702d2090781738351edfebc4a75c3a0cbf
chpike <chpike@umich.edu>
2016-11-15 00:12:49 -0500

    Remove turn in directory

--- eecs-494-f16-p4_acliu_yhpham_chpike_shanesms/eecs-494-f16-p4_acliu_yhpham_chpike_shanesms_Repo/Assets/Scripts/ObjectScripts/GravityGenerator.cs
+++
@@ -1,14 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GravityGenerator : ObjectBase {
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (!broken) {
-            //remove negative effects            GetComponent<Rigidbody>().useGravity = true;
-            this.enabled = false;
-        }
-	}
-
-}
