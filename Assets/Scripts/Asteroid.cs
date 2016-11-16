ef2bd0a50e03d0a15ae05037354432160f905581
yhpham <yelenapham@gmail.com>
2016-11-16 12:20:06 -0500

    add asteroid movement script

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour {
 
+	public float speed;
+	public float minX;
+	public float maxX;
+
 	void Start () {
 	
 	}
 
-	void Update () {
-	
+	void FixedUpdate () {
+		Move();
 	}
 
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.tag == "Bullet1" || col.gameObject.tag == "Bullet2") {
 			transform.localScale -= new Vector3(1, 1, 1);
 		}
+	}
+
+	void Move() {
+		if (transform.position.x < minX || transform.position.x > maxX) {
+			speed *= -1;
+		}
+
+		GetComponent<Rigidbody>().velocity = Quaternion.Euler(0, 0, 30) * Vector3.right * speed;
 	}
 }
