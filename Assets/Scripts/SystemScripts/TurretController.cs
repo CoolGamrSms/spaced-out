324d858b4d5e942dc2f3d48e90c2b22e78d5aa34
yhpham <yelenapham@gmail.com>
2016-11-27 16:28:59 -0500

    bullets die

--- Assets/Scripts/SystemScripts/TurretController.cs
+++ Assets/Scripts/SystemScripts/TurretController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using InControl;
 
-
 public class TurretController : Engineer {
 
     public GameObject pBullet;
@@ -17,34 +16,36 @@ public class TurretController : Engineer {
     List<Transform> bulletSpawns = new List<Transform>();
     ShipController sc;
 
-    // Use this for initialization
     void Start() {
         sc = GetComponentInParent<ShipController>();
+        
         for (int i = 0; i < transform.childCount; ++i) {
             Transform child = tilt.GetChild(i);
+            
             if (child.name == "BulletSpawnPoint") {
                 bulletSpawns.Add(child);
             }
         }
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
         if (eController.Action1.IsPressed && timer > cooldownLimit) {
             foreach (Transform pos in bulletSpawns) {
                 GameObject bullet = Instantiate(pBullet);
                 bullet.transform.rotation = pos.rotation;
                 bullet.transform.position = pos.position;
+
+                Destroy(bullet, 2.0f);
             }
+
             timer = 0f;
         }
+
 		timer += Time.deltaTime;
 
         if (sc.commandCenterBroken) return;
+
         transform.RotateAround(transform.position, transform.up, eController.LeftStickX.Value*turnRate);
         tilt.transform.RotateAround(tilt.transform.position, tilt.transform.right, -eController.LeftStickY.Value*tiltRate);
-		//print (eController.LeftStickX.Value);
-
-
     }
 }
