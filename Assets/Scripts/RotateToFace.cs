3e2bf8cdf2b056da84dcebf7337c058f47dc7348
yhpham <yelenapham@gmail.com>
2016-11-29 23:09:17 -0500

    add component labels

--- Assets/Scripts/RotateToFace.cs
+++ Assets/Scripts/RotateToFace.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class RotateToFace : MonoBehaviour {
 
     public GameObject face;
+    public bool isEngineText = false;
 
 	void Update () {
         var targetPosition = face.transform.position;
 
         targetPosition.y = transform.position.y;
         transform.LookAt(targetPosition);
+
+        if (isEngineText) {
+        	transform.Rotate(0, 180, 0);
+        }
 	}
 }
