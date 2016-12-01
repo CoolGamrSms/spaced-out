d84ea78625722df2c62a427344613d7917484386
Amy Liu <amyliu38@gmail.com>
2016-11-30 20:41:06 -0500

    hull breach changes

--- Assets/Scripts/SystemScripts/TurretController.cs
+++ Assets/Scripts/SystemScripts/TurretController.cs
@@ -16,12 +16,14 @@ public class TurretController : Engineer
     public float tiltRate;
     List<Transform> bulletSpawns = new List<Transform>();
     ShipController sc;
-    float mypitch;
+	Rigidbody shipRb;
+	float mypitch;
 
     void Start()
     {
         shoot = GetComponent<AudioSource>();
         sc = GetComponentInParent<ShipController>();
+		shipRb = GetComponentInParent<Rigidbody> ();
         mypitch = shoot.pitch;
 
         for (int i = 0; i < transform.childCount; ++i)
@@ -46,7 +48,7 @@ public class TurretController : Engineer
                 GameObject bullet = Instantiate(pBullet);
                 bullet.transform.rotation = pos.rotation;
                 bullet.transform.position = pos.position;
-                bullet.GetComponent<TurretBullet>().shipV = GetComponentInParent<Rigidbody>().velocity;
+                bullet.GetComponent<TurretBullet>().shipV = shipRb.velocity;
             }
 
             timer = 0f;
@@ -57,6 +59,11 @@ public class TurretController : Engineer
         if (sc.commandCenterBroken) return;
 
         transform.RotateAround(transform.position, transform.up, eController.LeftStickX.Value * turnRate);
+		/*float xRot = transform.localRotation.eulerAngles.x;
+		xRot = Mathf.Clamp(xRot, 85f, 275f);
+		transform.localRotation = Quaternion.Euler(xRot, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);*/
+		Debug.Log (transform.localRotation.eulerAngles.x);
+
         tilt.transform.RotateAround(tilt.transform.position, tilt.transform.right, -eController.LeftStickY.Value * tiltRate);
 
     }
